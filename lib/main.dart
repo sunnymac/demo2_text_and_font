9b18c23e6e14340aed260c5cafe474169c20f7f3
sunnymac <sanjaymakwana.it@gmail.com>
2021-10-25 14:51:31 +0530

    google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text("Demo2 Text and Fonts"),
+          centerTitle: true,
+        ),
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -26,17 +31,14 @@ class MyApp extends StatelessWidget {
                 letterSpacing: 2.0,
               ),
             ),
-            Text(
-              "Text Change using Asset Fonts",
-              style: TextStyle(
-                fontFamily: "Festive",
-                fontSize: 30,
-                fontWeight: FontWeight.w100,
-                fontStyle: FontStyle.italic,
-                color: Colors.red,
-                letterSpacing: 2.0,
-              ),
-            ),
+            Text("Text using Google Fonts",
+                style: GoogleFonts.robotoMono(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.red,
+                  letterSpacing: 2.0,
+                )),
           ],
         )),
       ),
